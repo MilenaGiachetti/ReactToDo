5dbe591beb24a155ae930c5fbdc1021e025e8b51
MilenaGiachetti <giachettimilena@gmail.com>
2020-09-16 21:08:24 -0300

    Updates

---
+++ precache-manifest.c963d6968d27f51d0f22f78ce25bb648.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abee33aa96f82770ca034145a5e15c7e",
+    "url": "/ReactToDo/index.html"
+  },
+  {
+    "revision": "d265a4440be721cac25c",
+    "url": "/ReactToDo/static/css/main.b75c54c6.chunk.css"
+  },
+  {
+    "revision": "87ebd8998be5c6029523",
+    "url": "/ReactToDo/static/js/2.fa5f13a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactToDo/static/js/2.fa5f13a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d265a4440be721cac25c",
+    "url": "/ReactToDo/static/js/main.fda62f0e.chunk.js"
+  },
+  {
+    "revision": "e68e20cc72b560b6167c",
+    "url": "/ReactToDo/static/js/runtime-main.a9aa282f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactToDo/static/media/logo.ee7cd8ed.svg"
+  }
+]);
